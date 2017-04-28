2f0d4907d8b3ac9464bf43212d2a023b9c936ba4
priyankaselvamariyal <priyankaselvamariyal@gmail.com>
2017-04-28 13:16:35 +0530

    Create prog15.c

---
+++ prog15.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num,m=20.n=40;
+clrscr();
+printf("print even numbers");
+for(num=m;num<=n,num++)
+{
+if(num%2==0)
+{
+printf("%d",num);
+}
+}
+getch();
+}
